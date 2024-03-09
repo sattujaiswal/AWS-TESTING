f84ab2bca2f05977367b5de09520b18226dedcd3
satyendra jaiswal <sattujaiswal@gmail.com>
2024-03-09 11:26:21 +0530

    4th commit

--- Hello.py
+++ Hello.py
@@ -11,3 +11,18 @@ if is_leap_year(year):
     print(year, "is a leap year.")
 else:
     print(year, "is not a leap year.")
+# Function to check if a number is even
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+# Taking user input
+num = int(input("Enter a number: "))
+
+# Checking if the number is even
+if is_even(num):
+    print(num, "is an even number.")
+else:
+    print(num, "is not an even number.")
