18acedbf7bbbdd75fff2463a42262fe93820c89a
Satyendra Jaiswal <sattujaiswal@gmail.com>
2024-06-05 01:59:38 +0530

    updated

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = FastAPI()
 @app.get("/")
 def print_numbers():
     numbers = [str(i) for i in range(1, 11)]
-    return "\n".join(numbers)
+    return " ".join(numbers)
+
+
+@app.get("/{name}/")  # Corrected route path
+def print_numbers(name):
+    return f"Hi {name}"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
