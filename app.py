ce24a84f25b56660dd0a1a739484a6350eee09bb
Satyendra Jaiswal <sattujaiswal@gmail.com>
2024-06-05 23:50:28 +0530

    changed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import time
 
 app = FastAPI()
 
@@ -8,6 +9,15 @@ def print_numbers():
     numbers = [str(i) for i in range(1, 11)]
     return " ".join(numbers)
 
+
+@app.get("/compute/")
+def compute():
+    # Simulate CPU-intensive task
+    start_time = time.time()
+    while time.time() - start_time < 10:  # Run for 10 seconds
+        pass
+    return {"message": "Completed CPU-intensive task"}
+    
 
 @app.get("/{name}/")  # Corrected route path
 def print_numbers(name):
